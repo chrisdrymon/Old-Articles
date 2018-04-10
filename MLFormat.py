b48d0f6d4a1eb20bbe7fdfc056ef6f4617637f37
Chris Drymon <chrisdrymon@yahoo.com>
2018-04-10 14:14:20 -0500

    It's working for PROIEL! Can now output to a Pandas Dataframe or CSV.

--- MLFormat.py
+++ MLFormat.py
@@ -4,7 +4,7 @@ import os
 import pandas as pd
 
 
-def proieltbs(treebank):
+def proieltbs(treebank, perarticledict, totarticlenumber):
     """Creates lists in ML format for each article."""
     froot = treebank.getroot()
     for source in froot:
@@ -13,11 +13,10 @@ def proieltbs(treebank):
                 alltokesinsent = sentence.findall('token')
                 for token in alltokesinsent:
                     if token.get('lemma') == 'ὁ':
-                        mlformatlist = []
                         form = token.get('form')
                         morph = token.get('morphology')
                         articlenumber = alltokesinsent.index(token)
-                        mlformatlist.extend([form, morph])
+                        mlformatlist = [form, morph]
                         i = -7
                         while i < 14:
                             nextwordid = articlenumber + i
@@ -29,27 +28,31 @@ def proieltbs(treebank):
                             except IndexError:
                                 mlformatlist.extend([None, None, None])
                             i += 1
-                        print(mlformatlist)
-
+                        perarticledict[totarticlenumber] = mlformatlist
+                        totarticlenumber += 1
+    returnlist = [perarticledict, totarticlenumber]
+    return returnlist
 
 os.chdir('/home/chris/Desktop/CustomTB')
 indir = os.listdir('/home/chris/Desktop/CustomTB')
-
+perArticleDict = {}
+totArticleNumber = 1
 for file_name in indir:
     if not file_name == 'README.md' and not file_name == '.git':
         tb = ET.parse(file_name)
         tbroot = tb.getroot()
         print(file_name)
         if tbroot.tag == 'proiel':
-            proieltbs(tb)
-        if tbroot.tag == 'treebank':
-            perseustbs(tb)
+            returnedList = proieltbs(tb, perArticleDict, totArticleNumber)
+            perArticleDict = returnedList[0]
+            totArticleNumber = returnedList[1]
+#        if tbroot.tag == 'treebank':
+ #           perseustbs(tb, perArticleDict, totArticleNumber)
 
-#df = pd.DataFrame.from_dict(artPos, orient='index')
+df = pd.DataFrame.from_dict(perArticleDict, orient='index')
+outname = 'MLFormat.csv'
+outdir = '/home/chris/Desktop'
+outpath = os.path.join(outdir, outname)
+df.to_csv(outpath)
 
-#outname = 'MLFormat.csv'
-#outdir = '/home/chris/Desktop'
-#outpath = os.path.join(outdir, outname)
-#df.to_csv(outpath)
 
-#print(artPos)
