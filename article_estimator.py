89f0fa37f33be8aef028dbaf8e97c7dbf7c9e13e
Chris Drymon <chrisdrymon@yahoo.com>
2018-04-14 18:02:04 -0500

    Made it as simple as I could.

--- article_estimator.py
+++ article_estimator.py
@@ -1,43 +1,81 @@
 """An Example of a DNNClassifier for the Article dataset."""
-import argparse
 import tensorflow as tf
-
-import article_data
-
-
-parser = argparse.ArgumentParser()
-parser.add_argument('--batch_size', default=100, type=int, help='batch size')
-parser.add_argument('--train_steps', default=30, type=int,
-                    help='number of training steps')
+import pandas as pd
 
 
-def main(argv):
-    args = parser.parse_args(argv[1:])
+def load_data(y_name='Answer'):
+    """Returns the article dataset as (train_x, train_y), (test_x, test_y)."""
 
-    # Fetch the data
-    (train_x, train_y), (test_x, test_y) = article_data.load_data()
+    train_path = '/home/chris/Desktop/SmallMLTrain.csv'
+    test_path = '/home/chris/Desktop/SmallMLTest.csv'
 
-    # Categorical Columns wrapped in Indicator Columns
-    my_feature_columns = []
-    for key in train_x.keys():
-        temp_column = tf.feature_column.categorical_column_with_vocabulary_file(key=key, vocabulary_file='/home/chris/Desktop/Everythinglist.txt', default_value=0)
-        my_feature_columns.append(tf.feature_column.indicator_column(temp_column))
+    train = pd.read_csv(train_path)
 
-    classifier = tf.estimator.DNNClassifier(feature_columns=my_feature_columns,
-                                            hidden_units=[200, 50, 50], n_classes=6, model_dir='/home/chris/Desktop/logger')
+    train_x, train_y = train, train.pop(y_name)
 
-    # Train the Model.
-    classifier.train(
-        input_fn=lambda:article_data.train_input_fn(train_x, train_y, args.batch_size),
-        steps=args.train_steps)
+    test = pd.read_csv(test_path)
+    test_x, test_y = test, test.pop(y_name)
 
-    # Evaluate the model.
-    eval_result = classifier.evaluate(
-        input_fn=lambda:article_data.eval_input_fn(test_x, test_y, args.batch_size))
-
-    print('\nTest set accuracy: {accuracy:0.3f}\n'.format(**eval_result))
+    return (train_x, train_y), (test_x, test_y)
 
 
-if __name__ == '__main__':
-    tf.logging.set_verbosity(tf.logging.INFO)
-    tf.app.run(main)
+def train_input_fn(features, labels, batch_size):
+    """An input function for training"""
+    # Convert the inputs to a Dataset.
+    dataset = tf.data.Dataset.from_tensor_slices((dict(features), labels))
+
+    # Shuffle, repeat, and batch the examples.
+    dataset = dataset.shuffle(5000).repeat().batch(batch_size)
+
+    # Return the dataset.
+    return dataset
+
+
+def eval_input_fn(features, labels, batch_size):
+    """An input function for evaluation or prediction"""
+    features=dict(features)
+    if labels is None:
+        # No labels, use only features.
+        inputs = features
+    else:
+        inputs = (features, labels)
+
+    # Convert the inputs to a Dataset.
+    dataset = tf.data.Dataset.from_tensor_slices(inputs)
+
+    # Batch the examples
+    assert batch_size is not None, "batch_size must not be None"
+    dataset = dataset.batch(batch_size)
+
+    # Return the dataset.
+    return dataset
+
+
+tf.logging.set_verbosity(tf.logging.INFO)
+batchSize = 100
+trainSteps = 300
+
+# Fetch the data
+(train_X, train_Y), (test_X, test_Y) = load_data()
+
+# Categorical Columns wrapped in Indicator Columns
+my_feature_columns = []
+for key in train_X.keys():
+    temp_column = tf.feature_column.\
+        categorical_column_with_vocabulary_file(key=key, vocabulary_file='/home/chris/Desktop/Everythinglist.txt',
+                                                default_value=0)
+    my_feature_columns.append(tf.feature_column.indicator_column(temp_column))
+
+classifier = tf.estimator.DNNClassifier(feature_columns=my_feature_columns,
+                                        hidden_units=[200, 50, 50], n_classes=6, model_dir='/home/chris/Desktop/logger')
+
+# Train the Model.
+classifier.train(
+    input_fn=lambda:train_input_fn(train_X, train_Y, batchSize),
+    steps=trainSteps)
+
+# Evaluate the model.
+eval_result = classifier.evaluate(
+    input_fn=lambda:eval_input_fn(test_X, test_Y, batchSize))
+
+print('\nTest set accuracy: {accuracy:0.3f}\n'.format(**eval_result))
