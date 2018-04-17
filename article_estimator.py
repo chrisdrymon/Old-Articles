01c10ab51bfa4b02e77c3279bceb038bca16c68e
Chris Drymon <chrisdrymon@yahoo.com>
2018-04-17 16:45:24 -0500

    Correctly silenced to a good degree now, while regularly providing loss information.

--- article_estimator.py
+++ article_estimator.py
@@ -4,6 +4,8 @@ import pandas as pd
 import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
+tf.logging.set_verbosity(tf.logging.INFO)
+
 
 def load_data(y_name='Answer'):
     """Returns the article dataset as (train_x, train_y), (test_x, test_y)."""
@@ -12,7 +14,6 @@ def load_data(y_name='Answer'):
     test_path = '/home/chris/Desktop/MLTest.csv'
 
     train = pd.read_csv(train_path)
-
     train_x, train_y = train, train.pop(y_name)
 
     test = pd.read_csv(test_path)
@@ -53,8 +54,9 @@ def eval_input_fn(features, labels, batch_size):
 (train_X, train_Y), (test_X, test_Y) = load_data()
 
 elementCount = train_X.shape[0]
+print(elementCount, "training elements.")
 batchSize = 20
-epochs = 2
+epochs = 5
 trainSteps = int(epochs * elementCount / batchSize)
 
 my_feature_columns = []
@@ -64,12 +66,16 @@ for key in train_X.keys():
                                                 default_value=0)
     my_feature_columns.append(tf.feature_column.indicator_column(temp_column))
 
-classifier = tf.estimator.DNNClassifier(feature_columns=my_feature_columns,
-                                        hidden_units=[10, 10], n_classes=6,
-                                        model_dir='/home/chris/Desktop/MultiLog/bs20-10x10')
-j = 0
+classifier = tf.estimator.DNNClassifier(feature_columns=my_feature_columns, hidden_units=[40, 40], n_classes=6,
+                                        model_dir='/home/chris/Desktop/TensLog/bs20-40x40')
+
+j = 5
 while j < epochs:
+
     classifier.train(input_fn=lambda: train_input_fn(train_X, train_Y, elementCount, batchSize), steps=trainSteps)
+
     eval_result = classifier.evaluate(input_fn=lambda: eval_input_fn(test_X, test_Y, batchSize))
-    print('\nEpoch', j, 'test set accuracy: {accuracy:0.3f}\n'.format(**eval_result))
+
+    print('\nEpoch', (j+1), 'test set accuracy: {accuracy:0.3f}\n'.format(**eval_result))
+
     j += 1
