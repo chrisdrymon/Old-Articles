5ed47d367c766b43474ff3ef5b23803c4844e2ee
Chris Drymon <chrisdrymon@yahoo.com>
2018-04-17 15:29:29 -0500

    Quieted TF Warnings. Added a loop to get some feedback.

--- article_estimator.py
+++ article_estimator.py
@@ -1,6 +1,8 @@
 """An Example of a DNNClassifier for the Article dataset."""
 import tensorflow as tf
 import pandas as pd
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
 
 def load_data(y_name='Answer'):
@@ -10,8 +12,6 @@ def load_data(y_name='Answer'):
     test_path = '/home/chris/Desktop/MLTest.csv'
 
     train = pd.read_csv(train_path)
-    element_count = train.shape[0]-1
-    print(element_count)
 
     train_x, train_y = train, train.pop(y_name)
 
@@ -23,13 +23,11 @@ def load_data(y_name='Answer'):
 
 def train_input_fn(features, labels, element_count, batch_size):
     """An input function for training"""
-    # Convert the inputs to a Dataset.
+
     dataset = tf.data.Dataset.from_tensor_slices((dict(features), labels))
 
-    # Shuffle, repeat, and batch the examples.
     dataset = dataset.shuffle(element_count).repeat().batch(batch_size)
 
-    # Return the dataset.
     return dataset
 
 
@@ -37,15 +35,14 @@ def eval_input_fn(features, labels, batch_size):
     """An input function for evaluation or prediction"""
     features = dict(features)
     if labels is None:
-        # No labels, use only features.
+        # If no labels, use only features.
         inputs = features
+        print("An input had no label!")
     else:
         inputs = (features, labels)
 
-    # Convert the inputs to a Dataset.
     dataset = tf.data.Dataset.from_tensor_slices(inputs)
 
-    # Batch the examples
     assert batch_size is not None, "batch_size must not be None"
     dataset = dataset.batch(batch_size)
 
@@ -53,17 +50,13 @@ def eval_input_fn(features, labels, batch_size):
     return dataset
 
 
-tf.logging.set_verbosity(tf.logging.FATAL)
-
-# Fetch the data
 (train_X, train_Y), (test_X, test_Y) = load_data()
 
 elementCount = train_X.shape[0]
-batchSize = 100
-epochs = .1
+batchSize = 20
+epochs = 2
 trainSteps = int(epochs * elementCount / batchSize)
 
-# Categorical Columns wrapped in Indicator Columns
 my_feature_columns = []
 for key in train_X.keys():
     temp_column = tf.feature_column.\
@@ -72,16 +65,11 @@ for key in train_X.keys():
     my_feature_columns.append(tf.feature_column.indicator_column(temp_column))
 
 classifier = tf.estimator.DNNClassifier(feature_columns=my_feature_columns,
-                                        hidden_units=[2, 500, 500], n_classes=6,
-                                        model_dir='/home/chris/Desktop/MultiLog/bs100-2x500x500')
-
-# Train the Model.
-classifier.train(
-    input_fn=lambda: train_input_fn(train_X, train_Y, elementCount, batchSize),
-    steps=trainSteps)
-
-# Evaluate the model.
-eval_result = classifier.evaluate(
-    input_fn=lambda: eval_input_fn(test_X, test_Y, batchSize))
-
-print('\nTest set accuracy: {accuracy:0.3f}\n'.format(**eval_result))
+                                        hidden_units=[10, 10], n_classes=6,
+                                        model_dir='/home/chris/Desktop/MultiLog/bs20-10x10')
+j = 0
+while j < epochs:
+    classifier.train(input_fn=lambda: train_input_fn(train_X, train_Y, elementCount, batchSize), steps=trainSteps)
+    eval_result = classifier.evaluate(input_fn=lambda: eval_input_fn(test_X, test_Y, batchSize))
+    print('\nEpoch', j, 'test set accuracy: {accuracy:0.3f}\n'.format(**eval_result))
+    j += 1
