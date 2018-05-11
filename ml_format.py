6777dfec65832de9303371672af7681bad8b8fa9
Chris Drymon <chrisdrymon@yahoo.com>
2018-05-11 15:43:43 -0500

    Removed "morphology" in much the same manner as 'form.' It was redundant as each individual letter of the morphology will already be included.

--- ml_format.py
+++ ml_format.py
@@ -5,7 +5,7 @@ from utility import deaccent
 from pathlib import Path
 
 
-def proieltbs(treebank, perarticledict, totarticlenumber, alllemmas, allmorphs, allletters, answersdict, allpos):
+def proieltbs(treebank, perarticledict, totarticlenumber, alllemmas, allletters, answersdict, allpos):
     """Creates lists in ML format for each article."""
     froot = treebank.getroot()
     for source in froot:
@@ -17,8 +17,6 @@ def proieltbs(treebank, perarticledict, totarticlenumber, alllemmas, allmorphs, 
                     # Create lists of words or letters.
                     if not deaccent(token.get('lemma')) in alllemmas:
                         alllemmas.append(deaccent(token.get('lemma')))
-                    if not token.get('morphology') in allmorphs:
-                        allmorphs.append(token.get('morphology'))
                     if not token.get('part-of-speech') in allpos:
                         allpos.append(token.get('part-of-speech'))
                     for letter in token.get('morphology'):
@@ -33,7 +31,7 @@ def proieltbs(treebank, perarticledict, totarticlenumber, alllemmas, allmorphs, 
                             jewish = 'yes'
                         else:
                             jewish = 'no'
-                        mlformatlist = [jewish, morph, pos]
+                        mlformatlist = [jewish, pos]
                         for letter in morph:
                             mlformatlist.append(letter)
                         headwordplace = int(token.get('head-id')) - int(token.get('id'))
@@ -42,11 +40,11 @@ def proieltbs(treebank, perarticledict, totarticlenumber, alllemmas, allmorphs, 
                             lemma = deaccent(alltokesinsent[nextwordid].get('lemma'))
                             morph = alltokesinsent[nextwordid].get('morphology')[:8]
                             pos = alltokesinsent[nextwordid].get('part-of-speech')
-                            mlformatlist.extend([lemma, morph, pos])
+                            mlformatlist.extend([lemma, pos])
                             for letter in morph:
                                 mlformatlist.append(letter)
                         except IndexError:
-                            mlformatlist.extend(['ellipsed']*11)
+                            mlformatlist.extend(['ellipsed']*10)
                         i = 1
                         while i < 5:
                             nextwordid = articlenumber + i
@@ -54,11 +52,11 @@ def proieltbs(treebank, perarticledict, totarticlenumber, alllemmas, allmorphs, 
                                 lemma = deaccent(alltokesinsent[nextwordid].get('lemma'))
                                 morph = alltokesinsent[nextwordid].get('morphology')[:8]
                                 pos = alltokesinsent[nextwordid].get('part-of-speech')
-                                mlformatlist.extend([lemma, morph, pos])
+                                mlformatlist.extend([lemma, pos])
                                 for letter in morph:
                                     mlformatlist.append(letter)
                             except IndexError:
-                                mlformatlist.extend(['ellipsed']*11)
+                                mlformatlist.extend(['ellipsed']*10)
                             i += 1
                         if headwordplace < -1 or headwordplace > 4:
                             fanswer = 5
@@ -68,86 +66,20 @@ def proieltbs(treebank, perarticledict, totarticlenumber, alllemmas, allmorphs, 
                         perarticledict[totarticlenumber] = mlformatlist
                         totarticlenumber += 1
 
-    returnlist = [perarticledict, totarticlenumber, alllemmas, allmorphs, allletters, allpos]
+    returnlist = [perarticledict, totarticlenumber, alllemmas, allpos, allletters]
     return returnlist
 
 
-def perseustbs(treebank, perarticledict, totarticlenumber, alllemmas, allmorphs, allletters, answersdict):
-    """Creates lists in ML format for each article."""
-    froot = treebank.getroot()
-    for body in froot:
-        for sentence in body:
-            alltokesinsent = sentence.findall(".*[@form]")
-            # Loops through every word.
-            for word in alltokesinsent:
-                # Create lists of words or letters.
-                if not deaccent(word.get('lemma')) in alllemmas:
-                    alllemmas.append(deaccent(word.get('lemma')))
-                if not deaccent(word.get('form')) in allforms:
-                    allforms.append(deaccent(word.get('form')))
-                if not word.get('postag') in allmorphs:
-                    allmorphs.append(word.get('postag'))
-                for letter in word.get('postag'):
-                    if letter not in allletters:
-                        allletters.append(letter)
-                # Creates all the values that will go into a single element.
-                if word.get('lemma') == 'ὁ':
-                    form = deaccent(word.get('form'))
-                    morph = word.get('postag')
-                    articlenumber = alltokesinsent.index(word)
-                    if body.get('jewish') == 'yes':
-                        jewish = 'yes'
-                    else:
-                        jewish = 'no'
-                    mlformatlist = [form, morph, jewish]
-                    for letter in morph:
-                        mlformatlist.append(letter)
-                    headwordplace = int(word.get('head-id')) - int(word.get('id'))
-                    i = -1
-                    while i < 0:
-                        nextwordid = articlenumber + i
-                        try:
-                            form = deaccent(alltokesinsent[nextwordid].get('form'))
-                            lemma = deaccent(alltokesinsent[nextwordid].get('lemma'))
-                            morph = alltokesinsent[nextwordid].get('postag')
-                            mlformatlist.extend([form, lemma, morph])
-                            for letter in morph:
-                                mlformatlist.append(letter)
-                        except IndexError:
-                            mlformatlist.extend(['ellipsed']*13)
-                        i += 1
-                    i += 1
-                    while i < 4:
-                        nextwordid = articlenumber + i
-                        try:
-                            form = deaccent(alltokesinsent[nextwordid].get('form'))
-                            lemma = deaccent(alltokesinsent[nextwordid].get('lemma'))
-                            morph = alltokesinsent[nextwordid].get('postag')
-                            mlformatlist.extend([form, lemma, morph])
-                            for letter in morph:
-                                mlformatlist.append(letter)
-                        except IndexError:
-                            mlformatlist.extend(['ellipsed']*12)
-                        i += 1
-                    if headwordplace < -2 or headwordplace > 3:
-                        fanswer = 5
-                    else:
-                        fanswer = answersdict[headwordplace]
-                    mlformatlist.append(fanswer)
-                    perarticledict[totarticlenumber] = mlformatlist
-                    totarticlenumber += 1
+def perseustbs(treebank, perarticledict, totarticlenumber, alllemmas, allletters, answersdict, allpos):
 
-    returnlist = [perarticledict, totarticlenumber, alllemmas, allmorphs, allletters]
+    returnlist = [perarticledict, totarticlenumber, alllemmas, allpos, allletters]
     return returnlist
-
 
 treebankFolder = '/home/chris/Desktop/CustomTB/'
 
 outTrainPath = Path('/home/chris/Desktop/MLTrain.csv')
 outTestPath = Path('/home/chris/Desktop/MLTest.csv')
 lemmaListPath = Path('/home/chris/Desktop/Lemmalist.txt')
-formListPath = Path('/home/chris/Desktop/Formlist.txt')
-morphListPath = Path('/home/chris/Desktop/Morphlist.txt')
 lettersListPath = Path('/home/chris/Desktop/Letterslist.txt')
 ultimateListPath = Path('/home/chris/Desktop/Everythinglist.txt')
 
@@ -157,8 +89,6 @@ indir = os.listdir(treebankFolder)
 perArticleDict = {}
 totArticleNumber = 1
 allLemmas = ['yes', 'no']
-allForms = []
-allMorphs = []
 allLetters = []
 allPOS = []
 answersDict = {-1: 0,
@@ -174,30 +104,26 @@ for file_name in indir:
         tbroot = tb.getroot()
         print(file_name)
         if tbroot.tag == 'proiel':
-            returnedList = proieltbs(tb, perArticleDict, totArticleNumber, allLemmas, allMorphs, allLetters,
-                                     answersDict, allPOS)
+            returnedList = proieltbs(tb, perArticleDict, totArticleNumber, allLemmas, allPOS, allLetters, answersDict)
         else:
-            returnedList = perseustbs(tb, perArticleDict, totArticleNumber, allLemmas, allMorphs, allLetters,
-                                      answersDict)
+            returnedList = perseustbs(tb, perArticleDict, totArticleNumber, allLemmas, allPOS, allLetters, answersDict)
 
         perArticleDict = returnedList[0]
         totArticleNumber = returnedList[1]
         allLemmas = returnedList[2]
-        allForms = returnedList[3]
-        allMorphs = returnedList[4]
-        allLetters = returnedList[5]
+        allPOS = returnedList[3]
+        allLetters = returnedList[4]
 
-labelList = ['Jewish', 'Morph', 'POS', 'Person', 'Number', 'Tense', 'Mood', 'Voice', 'Gender', 'Case', 'Degree',
-             '-1lemma', '-1morph', '-1POS', '-1person', '-1number', '-1tense', '-1mood', '-1voice', '-1gender',
-             '-1case', '-1degree']
-addedList = allLemmas + allMorphs + allLetters + allPOS
+labelList = ['Jewish', 'POS', 'Person', 'Number', 'Tense', 'Mood', 'Voice', 'Gender', 'Case', 'Degree', '-1lemma',
+             '-1POS', '-1person', '-1number', '-1tense', '-1mood', '-1voice', '-1gender', '-1case', '-1degree']
+
+addedList = allLemmas + allLetters + allPOS
 ultimateList = list(set(addedList))
 
 j = 1
 while j < 5:
     labelNumber = str(j)
     numLemma = labelNumber + 'lemma'
-    numMorph = labelNumber + 'morph'
     numPOS = labelNumber + 'POS'
     numPerson = labelNumber + 'person'
     numNumber = labelNumber + 'number'
@@ -207,7 +133,7 @@ while j < 5:
     numGender = labelNumber + 'gender'
     numCase = labelNumber + 'case'
     numDegree = labelNumber + 'degree'
-    newList = [numLemma, numMorph, numPOS, numPerson, numNumber, numTense, numMood, numVoice, numGender,
+    newList = [numLemma, numPOS, numPerson, numNumber, numTense, numMood, numVoice, numGender,
                numCase, numDegree]
     labelList.extend(newList)
     j += 1
@@ -225,9 +151,6 @@ dfTest.to_csv(outTestPath, index=False)
 with open(lemmaListPath, "w") as output:
     for s in allLemmas:
         output.write("%s\n" % s)
-with open(morphListPath, "w") as output:
-    for s in allMorphs:
-        output.write("%s\n" % s)
 with open(lettersListPath, "w") as output:
     for s in allLetters:
         output.write("%s\n" % s)
@@ -235,6 +158,5 @@ with open(ultimateListPath, "w") as output:
     for s in ultimateList:
         output.write("%s\n" % s)
 print(len(allLemmas), 'lemmas in lemma list.')
-print(len(allMorphs), 'morphologies in morph list.')
 print(len(ultimateList), 'entries total in the ultimate list.')
 print(len(perArticleDict), 'article entires.')
