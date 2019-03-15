bf9d5b7a19d4e42bcc59a5bf567f019a605c5b22
chris <chrisdrymon@yahoo.com>
2019-03-14 20:44:56 -0500

    Updated to actually work possibly

--- Keraripper.py
+++ Keraripper.py
@@ -7,10 +7,17 @@ print(tf.VERSION)
 print(tf.keras.__version__)
 
 model = tf.keras.Sequential([
-layers.Dense(20, activation='relu', input_shape=(100,)),
-layers.Dense(20, activation='relu'),
-layers.Dense(3, activation='softmax')])
+    layers.Dense(10, activation='relu', input_shape=(3,)),
+    layers.Dense(10, activation='relu'),
+    layers.Dense(3, activation='softmax')])
 
 model.compile(optimizer=tf.train.AdamOptimizer(0.001),
               loss='categorical_crossentropy',
               metrics=['accuracy'])
+
+train_samples = [['Tuesday', 'Paladin', 71.8], ['Tuesday', 'Rogue', 72.7], ['Tuesday', 'Warrior', 71.1], \
+                 ['Wednesday', 'Mage', 69.4]]
+
+train_labels = [2, 2, 0, 1]
+
+model.fit(x=train_samples, y=train_labels, batch_size=4, epochs=10, shuffle=True)
