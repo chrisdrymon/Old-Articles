d898bd21b6c3ae5bf83ea7c922a6be0bd9236797
chris <chrisdrymon@yahoo.com>
2019-03-21 01:13:46 -0500

    Lists are built for input layer.

--- Keraripper.py
+++ Keraripper.py
@@ -13,31 +13,31 @@ dayDict = {'Sunday': 0, 'Monday': 1, 'Tuesday': 2, 'Wednesday': 3, 'Thursday': 4
 classDict = {'Druid': 0, 'Hunter': 1, 'Mage': 2, 'Paladin': 3, 'Priest': 4, 'Rogue': 5, 'Shaman': 6,\
              'Warlock': 7, 'Warrior': 8}
 
-df=pd.read_csv('/home/chris/Desktop/KrippWins.csv', sep=',',header=None)
-print(df)
+df = pd.read_csv('/home/chris/Desktop/KrippWins.csv', sep=',',header=None)
+df.sample(frac=1)
 tempList = []
 for row in df.itertuples():
     hotNum = dayDict[row[1]]
-    hot = [0, 0, 0, 0, 0, 0, 0]
-    hot[hotNum] = 1
-    print(hot)
+    hot1 = [0, 0, 0, 0, 0, 0, 0]
+    hot1[hotNum] = 1
+    hotNum = classDict[row[2]]
+    hot2 = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+    hot2[hotNum] = 1
+    readyMat = hot1 + hot2
+    readyMat.append((row[3]-60)/20)
+    tempList.append(readyMat)
+formatted = np.array(tempList)
+print(formatted)
 
-#formatted = np.array()
+model = tf.keras.Sequential([
+layers.Dense(10, activation='relu', input_shape=(17,)),
+layers.Dense(10, activation='relu'),
+layers.Dense(3, activation='softmax')])
 
-#model = tf.keras.Sequential([
-#layers.Dense(10, activation='relu', input_shape=(17,)),
-#layers.Dense(10, activation='relu'),
-#layers.Dense(3, activation='softmax')])
+model.compile(optimizer=tf.train.AdamOptimizer(0.001),
+              loss='categorical_crossentropy',
+              metrics=['accuracy'])
 
-#model.compile(optimizer=tf.train.AdamOptimizer(0.001),
- #             loss='categorical_crossentropy',
- #             metrics=['accuracy'])
-
-#train_samples = np.array([[4, 3, 1], [3, 7, 0], [9, 2, 1], [8, 3, 2]])
-#train_samples = np.array([['Tuesday', 'Paladin', 71.8], ['Tuesday', 'Rogue', 72.7], ['Tuesday', 'Warrior', 71.1], \
- #                ['Wednesday', 'Mage', 69.4]])
-
-#y_binary = tf.keras.utils.to_categorical(['Tuesday'])
 #train_labels = np.array(y_binary)
 #print(train_labels)
 
