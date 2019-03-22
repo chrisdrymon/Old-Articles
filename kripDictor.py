47e0f4d35a671f54a47811792113335566a7c5e3
chris <chrisdrymon@yahoo.com>
2019-03-22 14:49:41 -0500

    Saving and Loading Models

--- kripDictor.py
+++ kripDictor.py
@@ -7,7 +7,9 @@ from tensorflow.keras import layers
 print(tf.VERSION)
 print(tf.keras.__version__)
 
-#Preparing dictionaries to convert data into integers. Later they will be turned to one-hots.
+# We're doing to use this to load a model and predict.
+
+# Preparing dictionaries to convert data into integers. Later they will be turned to one-hots.
 dayDict = {'Sunday': 0, 'Monday': 1, 'Tuesday': 2, 'Wednesday': 3, 'Thursday': 4,\
            'Friday': 5, 'Saturday': 6}
 classDict = {'Druid': 0, 'Hunter': 1, 'Mage': 2, 'Paladin': 3, 'Priest': 4, 'Rogue': 5, 'Shaman': 6,\
