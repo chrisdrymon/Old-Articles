86c2616084778f0f60ce8225579bb14df9692597
chris <chrisdrymon@yahoo.com>
2019-03-22 01:11:35 -0500

    Getting rid of dataframes. Make everything vanilla/numpy. Still work to do.

--- Keraripper.py
+++ Keraripper.py
@@ -16,6 +16,7 @@ classDict = {'Druid': 0, 'Hunter': 1, 'Mage': 2, 'Paladin': 3, 'Priest': 4, 'Rog
 df = pd.read_csv('/home/chris/Desktop/KrippWins.csv', sep=',',header=None)
 df.sample(frac=1)
 tempList = []
+
 for row in df.itertuples():
     hotNum = dayDict[row[1]]
     hot1 = [0, 0, 0, 0, 0, 0, 0]
@@ -23,11 +24,19 @@ for row in df.itertuples():
     hotNum = classDict[row[2]]
     hot2 = [0, 0, 0, 0, 0, 0, 0, 0, 0]
     hot2[hotNum] = 1
-    readyMat = hot1 + hot2
-    readyMat.append((row[3]-60)/20)
+    preNump = hot1 + hot2
+    preNump.append((row[3]-60)/20)
+    preNump.append(row[4])
+
     tempList.append(readyMat)
+splitNum = int(df.shape[0]*.8)
+trainData = df.head(splitNum)
+evalData = df.tail(df.shape[0]-splitNum)
+trainLabels = trainData.pop(3)
+evalLabels = evalData.pop(3)
+print(trainLabels)
+print(df.shape[0], trainData.shape[0], evalData.shape[0])
 formatted = np.array(tempList)
-print(formatted)
 
 model = tf.keras.Sequential([
 layers.Dense(10, activation='relu', input_shape=(17,)),
