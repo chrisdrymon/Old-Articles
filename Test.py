4634e919b124835690d84ae1f8e769f8a21330a7
chris <chrisdrymon@yahoo.com>
2019-04-19 23:40:15 -0500

    Soup worked. Got all his deck collection.

--- Test.py
+++ Test.py
@@ -1,1 +1,8 @@
-import tensorflow as tf
+import pandas as pd
+import requests
+
+df = pd.read_csv('/home/chris/Desktop/datedURLs2.csv')
+addresses = df['Address']
+newAdList = []
+for address in addresses:
+    print(address[38:44])
